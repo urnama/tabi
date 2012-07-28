73f11cd781b3d80f71bf2793d8d6f01cb39d8374
Brice Texier <burisu@oneiros.fr>
2012-07-28 12:34:30 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tabi::Application.config.secret_token = '6492024570c788bdcbd4a955792cb2aa2d78e0d9db5ef855a2c47fdc66760dce15add9230ca62001f70435e67e910cafecf2f032688193e1fdd0940419dca417'
